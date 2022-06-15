cfcefaae8c02d58651046a5f25e4922e907e1392
leocpx <leontin.atanasoaei@yahoo.com>
2022-06-15 14:02:59 +0200

    replaced updater

--- Launcher/ViewModels/MainWindowViewModel.cs
+++ Launcher/ViewModels/MainWindowViewModel.cs
@@ -7,7 +7,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using AutoUpdateViaGitHubRelease;
+using AutoUpdaterDotNET;
 
 
 namespace Launcher.ViewModels
@@ -18,38 +18,17 @@ namespace Launcher.ViewModels
         public event PropertyChangedEventHandler PropertyChanged;
 
         #region -- PRIVATE --
-        private Update update { get; set; }
-        private string GitHubUser { get; set; } = "cpx4000";
-        private string GitHubRepo { get; set; } = "https://github.com/leocpx/CakeFactory.git";
-        private string tempDir { get; set; } 
-        private Assembly assembly { get; set; }
-        private Version version { get; set; }
+
         #endregion
         #endregion
 
         #region -- CONSTRUCTOR --
         public MainWindowViewModel()
         {
-            tempDir = $"{Directory.GetCurrentDirectory()}/temp";
-            Directory.CreateDirectory(tempDir);
 
 
-            update = new Update();
-            update.PropertyChanged += Update_PropertyChanged;
-            assembly = Assembly.GetExecutingAssembly();
-            version = assembly.GetName().Version;
-            update.CheckDownloadNewVersionAsync(GitHubUser, GitHubRepo, version, tempDir);
         }
 
-        private void Update_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-        {
-            if (update.Available)
-            {
-                var destinationDir = Path.GetDirectoryName(assembly.Location);
-                update.StartInstall(destinationDir);
-                Application.Current.Shutdown();
-            }
-        }
         #endregion
     }
 }
