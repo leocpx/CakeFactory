2a71a634c835ecd469a370f299863c3826feee1a
leocpx <leontin.atanasoaei@yahoo.com>
2022-06-15 15:20:43 +0200

    v2

--- Launcher/ViewModels/MainWindowViewModel.cs
+++ Launcher/ViewModels/MainWindowViewModel.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using AutoUpdaterDotNET;
@@ -26,7 +27,26 @@ namespace Launcher.ViewModels
         public MainWindowViewModel()
         {
 
-
+            new Thread(
+                () =>
+                {
+                    Thread.Sleep(3000);
+                    Application.Current.Dispatcher.Invoke(
+                        () =>
+                        {
+                            AutoUpdater.ReportErrors = true;
+                            AutoUpdater.Synchronous = true;
+                            AutoUpdater.ShowSkipButton = true;
+                            AutoUpdater.ShowRemindLaterButton = true;
+                            AutoUpdater.HttpUserAgent = "AutoUpdater";
+                            AutoUpdater.RunUpdateAsAdmin = false;
+                            AutoUpdater.DownloadPath = Directory.GetCurrentDirectory()+"/updates";
+                            AutoUpdater.InstallationPath = Directory.GetCurrentDirectory();
+                            AutoUpdater.ClearAppDirectory = true;
+                            AutoUpdater.UpdateFormSize = new System.Drawing.Size(800, 450);
+                            AutoUpdater.Start("https://raw.githubusercontent.com/leocpx/CakeFactory/main/Launcher/version.xml");
+                        });
+                }).Start();
         }
 
         #endregion
