b5004d6c8dc7ddb126b3703e5ab45e126cc27c8a
leocpx <leontin.atanasoaei@yahoo.com>
2022-06-23 10:27:18 +0200

    sql config loader

---
+++ DBManager/ConfigLoader.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace DBManager
+{
+    public class ConfigLoader
+    {
+        public static Configurations LoadConfigs()
+        {
+            if (!File.Exists(DbClient.SqlConfigFilePath))
+                return CreateNewSqlConfigFile();
+
+            return DeserializeConfigFile();
+        }
+
+        private static Configurations DeserializeConfigFile()
+        {
+            var ser = new XmlSerializer(typeof(Configurations));
+            var sr = new StreamReader(DbClient.SqlConfigFilePath);
+            return ser.Deserialize(sr) as Configurations;
+        }
+
+        private static Configurations CreateNewSqlConfigFile()
+        {
+            var configObj = new Configurations()
+            {
+                SQLAddress = "DESKTOP-G7L0IKL",
+                SQLDataBase = "cakeFactory",
+                SQLUser = "sa",
+                SQLPass = "tre543tre",
+                CSV_SEPARATOR = ";", 
+                EnableDebugInfo = false
+            };
+
+            var ser = new XmlSerializer(typeof(Configurations));
+            var sr = new StreamWriter(DbClient.SqlConfigFilePath);
+            ser.Serialize(sr, configObj);
+
+            return configObj;
+        }
+
+    }
+}
---
+++ DBManager/Configurations.cs
@@ -0,0 +1,15 @@
+﻿using eLongMuSQL;
+
+namespace DBManager
+{
+    public class Configurations : IConfigurations
+    {
+        public bool EnableDebugInfo{ get; set; }
+        public string SQLAddress{ get; set; }
+        public string SQLDataBase{ get; set; }
+        public string SQLPass{ get; set; }
+        public string SQLUser{ get; set; }
+        public string CSV_SEPARATOR{ get; set; }
+    }
+
+}
