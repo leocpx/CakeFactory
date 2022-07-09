7de516e9ec7e568fd1dc392b7d84d2600d4fb3c0
leocpx <leontin.atanasoaei@yahoo.com>
2022-07-09 23:39:55 +0200

    moved IOrder to its own file

---
+++ CoreCake/Interfaces/IOrder.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Interfaces
+{
+    public interface IOrder
+    {
+        long id { get; set; }
+        long _finishedGoodId { get; set; }
+        long _startTime { get; set; }
+        long _workerId { get; set; }
+        bool _completed { get; set; }
+    }
+}
--- CoreCake/Interfaces/IScheduleItem.cs
+++ CoreCake/Interfaces/IScheduleItem.cs
@@ -10,13 +10,4 @@ namespace Core.Interfaces
     {
         IOrder Order { get; set; }
     }
-
-    public interface IOrder
-    {
-        long id { get; set; }
-        long _finishedGoodId { get; set; }
-        long _startTime { get; set; }
-        long _workerId { get; set; }
-        bool _completed { get; set; }
-    }
 }
